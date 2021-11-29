87dac3b827e9d0eb81ce0bd41cb039375dcd84d1
huseyinsaritas <hsynsrtss@gmail.com>
2021-11-29 18:24:48 +0300

    textTestData updated

--- src/lib/tests/data/text.ts
+++ src/lib/tests/data/text.ts
@@ -1,6 +1,6 @@
 export const textTestData = [
   { inputText: "bit", numberValue: 7629154, byteLength: 3, hexValue: "626974", binValue: "011101000110100101100010" },
-  { inputText: "matrix", numberValue: undefined, byteLength: 6, hexValue: "6d6174726978", binValue: "011110000110100101110010011101000110000101101101" },
+  { inputText: "matrix", numberValue: 132394287128941, byteLength: 6, hexValue: "6d6174726978", binValue: "011110000110100101110010011101000110000101101101" },
   {
     inputText: "bitmatrix",
     numberValue: undefined,
